518e66513ae12ed229f338501309c90fe2fdbca3
Morgan Gribbins <morganjng@gmail.com>
2022-09-10 23:57:17 -0400

    some more playing around

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yaml/yaml.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
 
 void main() {
   runApp(const HomePage());
@@ -9,7 +10,6 @@ void main() {
 
 class NavDrawer extends Drawer {
   const NavDrawer({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -27,6 +27,20 @@ class NavDrawer extends Drawer {
           onTap: () {
             Navigator.push(
                 context, MaterialPageRoute(builder: (context) => MapPage()));
+          }),
+      ListTile(
+          leading: const Icon(Icons.yard),
+          title: const Text("Plants"),
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => PlantsPage()));
+          }),
+      ListTile(
+          leading: const Icon(Icons.crib),
+          title: const Text("Gardens"),
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => GardensPage()));
           }),
       ListTile(
           leading: const Icon(Icons.developer_board),
@@ -79,21 +93,103 @@ class MapPage extends StatelessWidget {
   }
 }
 
-class DevPage extends StatelessWidget {
-  const DevPage({super.key});
+class PlantsPage extends StatelessWidget {
+  const PlantsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Dev Mode")),
+      appBar: AppBar(title: Text("Plants")),
       body: Center(child: const Text("placeholder")),
+    );
+  }
+}
+
+class GardensPage extends StatelessWidget {
+  const GardensPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Gardens")),
+      body: Center(child: const Text("placeholder")),
+    );
+  }
+}
+
+class DevPage extends StatefulWidget {
+  const DevPage({super.key});
+
+  @override
+  State<DevPage> createState() => DevState();
+}
+
+class DevState extends State<DevPage> {
+  int _page = 0;
+  bool bInit = false;
+
+  static List<Widget> gardens = List<Widget>.empty(growable: true);
+
+  static List<Widget> plants = List<Widget>.empty(growable: true);
+
+  static List<Widget> _pages = [
+    ListView(
+      padding: const EdgeInsets.all(8),
+      children: gardens,
+    ),
+    ListView(
+      padding: const EdgeInsets.all(8),
+      children: plants,
+    ),
+    const Center(child: Text("Hey"))
+  ];
+
+  void _setPage(int index) {
+    setState(() {
+      _page = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var dataYaml = loadYaml(File('data.yaml').readAsStringSync());
+    String title;
+    if (gardens.isEmpty) {
+      for (int i = 0; i < dataYaml["garden_keys"].length; i++) {
+        title = dataYaml["garden"][dataYaml["garden_keys"][i].toString()]
+                ["title"]
+            .toString();
+        gardens.add(ListTile(
+            // leading: const Icon(Icons.house),
+            title: Text(title),
+            trailing: const Icon(Icons.more_vert)));
+      }
+    }
+    if (plants.isEmpty) {
+      for (int i = 0; i < dataYaml["plant_keys"].length; i++) {
+        title = dataYaml["plant"][dataYaml["plant_keys"][i].toString()]
+                ["common"]
+            .toString();
+        plants.add(ListTile(
+          // leading: const Icon(Icons.park),
+          title: Text(title),
+          trailing: const Icon(Icons.more_vert),
+        ));
+      }
+    }
+    return Scaffold(
+      appBar: AppBar(title: Text("Dev Mode")),
+      body: _pages[_page],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ),
       bottomNavigationBar:
           BottomNavigationBar(items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(icon: Icon(Icons.crib), label: "Gardens"),
         BottomNavigationBarItem(icon: Icon(Icons.yard), label: "Plants"),
         BottomNavigationBarItem(
             icon: Icon(Icons.account_circle), label: "Account")
-      ]),
+      ], currentIndex: _page, onTap: _setPage),
     );
   }
 }
