633b9f802114427e0fe1ff331ee47a70e6f7a910
Morgan Gribbins <morganjng@gmail.com>
2022-11-10 09:42:42 -0500

    trying to fix vulnerabilities?

--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 var express = require("express"),
   app = express(),
-  morgan = require("morgan");
-fs = require("fs");
+  morgan = require("morgan"),
+  fs = require("fs");
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
-  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-  mongoURLLabel = "";
+var port = 8080,
+  ip = "0.0.0.0";
 
 Object.assign = require("object-assign");
 
