e3585f849ac50b4f07d47e785694fe7b73f6033e
Morgan Gribbins <morganjng@gmail.com>
2022-11-15 21:23:30 -0500

    erm :pensive:

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -63,22 +63,15 @@ class NavDrawer extends Drawer {
           leading: const Icon(Icons.yard),
           title: const Text("Plants"),
           onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => PlantsPage()));
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => data.plantsPage()));
           }),
       ListTile(
           leading: const Icon(Icons.crib),
           title: const Text("Gardens"),
           onTap: () {
             Navigator.push(context,
-                MaterialPageRoute(builder: (context) => GardensPage()));
-          }),
-      ListTile(
-          leading: const Icon(Icons.developer_board),
-          title: const Text("Dev Mode"),
-          onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => DevPage()));
+                MaterialPageRoute(builder: (context) => data.gardensPage()));
           }),
     ]));
   }
@@ -114,180 +107,180 @@ class MapPage extends StatelessWidget {
   }
 }
 
-class PlantsPage extends StatelessWidget {
-  const PlantsPage({super.key});
+// class PlantsPage extends StatelessWidget {
+//   const PlantsPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Plants")),
-      body: Center(child: const Text("placeholder")),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Plants")),
+//       body: Center(child: const Text("placeholder")),
+//     );
+//   }
+// }
 
-class GardensPage extends StatelessWidget {
-  const GardensPage({super.key});
+// class GardensPage extends StatelessWidget {
+//   const GardensPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Gardens")),
-      body: Center(child: const Text("placeholder")),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Gardens")),
+//       body: Center(child: const Text("placeholder")),
+//     );
+//   }
+// }
 
-class DevPage extends StatefulWidget {
-  const DevPage({super.key});
+// class DevPage extends StatefulWidget {
+//   const DevPage({super.key});
 
-  @override
-  State<DevPage> createState() => DevState();
-}
+//   @override
+//   State<DevPage> createState() => DevState();
+// }
 
-class DevState extends State<DevPage> {
-  int _page = 0;
-  bool bInit = false;
+// class DevState extends State<DevPage> {
+//   int _page = 0;
+//   bool bInit = false;
 
-  static List<Widget> garden_tiles = List<Widget>.empty(growable: true);
+//   static List<Widget> garden_tiles = List<Widget>.empty(growable: true);
 
-  static List<Widget> plant_tiles = List<Widget>.empty(growable: true);
+//   static List<Widget> plant_tiles = List<Widget>.empty(growable: true);
 
-  static List<Widget> _pages = [
-    ListView(
-      padding: const EdgeInsets.all(8),
-      children: garden_tiles,
-    ),
-    ListView(
-      padding: const EdgeInsets.all(8),
-      children: plant_tiles,
-    ),
-    const Center(child: Text("Hey"))
-  ];
+//   static List<Widget> _pages = [
+//     ListView(
+//       padding: const EdgeInsets.all(8),
+//       children: garden_tiles,
+//     ),
+//     ListView(
+//       padding: const EdgeInsets.all(8),
+//       children: plant_tiles,
+//     ),
+//     const Center(child: Text("Hey"))
+//   ];
 
-  void _setPage(int index) {
-    setState(() {
-      _page = index;
-    });
-  }
+//   void _setPage(int index) {
+//     setState(() {
+//       _page = index;
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (_serverData is Map<String, dynamic>) {
-      var sd = _serverData!;
-      String title;
-      if (garden_tiles.isEmpty) {
-        beds.values.forEach((b) => garden_tiles.add(ListTile(
-            title: Text(b.title),
-            trailing: const Icon(Icons.more_vert),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => GardenPage(b)));
-            })));
-      }
-      if (plant_tiles.isEmpty) {
-        plants.values.forEach((p) => plant_tiles.add(ListTile(
-            title: Text(p.commonName),
-            trailing: const Icon(Icons.more_vert),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => PlantPage(p)));
-            })));
-      }
-    }
-    return Scaffold(
-      appBar: AppBar(title: Text("Dev Mode")),
-      body: _pages[_page],
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-      ),
-      bottomNavigationBar:
-          BottomNavigationBar(items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.crib), label: "Gardens"),
-        BottomNavigationBarItem(icon: Icon(Icons.yard), label: "Plants"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle), label: "Account")
-      ], currentIndex: _page, onTap: _setPage),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     if (_serverData is Map<String, dynamic>) {
+//       var sd = _serverData!;
+//       String title;
+//       if (garden_tiles.isEmpty) {
+//         beds.values.forEach((b) => garden_tiles.add(ListTile(
+//             title: Text(b.title),
+//             trailing: const Icon(Icons.more_vert),
+//             onTap: () {
+//               Navigator.push(context,
+//                   MaterialPageRoute(builder: (context) => GardenPage(b)));
+//             })));
+//       }
+//       if (plant_tiles.isEmpty) {
+//         plants.values.forEach((p) => plant_tiles.add(ListTile(
+//             title: Text(p.commonName),
+//             trailing: const Icon(Icons.more_vert),
+//             onTap: () {
+//               Navigator.push(context,
+//                   MaterialPageRoute(builder: (context) => PlantPage(p)));
+//             })));
+//       }
+//     }
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Dev Mode")),
+//       body: _pages[_page],
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//       ),
+//       bottomNavigationBar:
+//           BottomNavigationBar(items: const <BottomNavigationBarItem>[
+//         BottomNavigationBarItem(icon: Icon(Icons.crib), label: "Gardens"),
+//         BottomNavigationBarItem(icon: Icon(Icons.yard), label: "Plants"),
+//         BottomNavigationBarItem(
+//             icon: Icon(Icons.account_circle), label: "Account")
+//       ], currentIndex: _page, onTap: _setPage),
+//     );
+//   }
+// }
 
-class GardenPage extends StatelessWidget {
-  Bed bed;
+// class GardenPage extends StatelessWidget {
+//   Bed bed;
 
-  GardenPage(this.bed) {
-    super.key;
-  }
+//   GardenPage(this.bed) {
+//     super.key;
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    var ps = List<Widget>.empty(growable: true);
-    ps.add(ListTile(
-        leading:
-            const Text("Plants", style: TextStyle(fontWeight: FontWeight.bold)),
-        trailing: Text(plants[bed.plants[0]]!.commonName),
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => PlantPage(plants[bed.plants[0]]!)));
-        }));
+//   @override
+//   Widget build(BuildContext context) {
+//     var ps = List<Widget>.empty(growable: true);
+//     ps.add(ListTile(
+//         leading:
+//             const Text("Plants", style: TextStyle(fontWeight: FontWeight.bold)),
+//         trailing: Text(plants[bed.plants[0]]!.commonName),
+//         onTap: () {
+//           Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                   builder: (context) => PlantPage(plants[bed.plants[0]]!)));
+//         }));
 
-    for (int i = 1; i < bed.plants.length; i++) {
-      ps.add(ListTile(
-          onTap: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => PlantPage(plants[bed.plants[i]]!)));
-          },
-          trailing: Text(plants[bed.plants[i]]!.commonName)));
-    }
+//     for (int i = 1; i < bed.plants.length; i++) {
+//       ps.add(ListTile(
+//           onTap: () {
+//             Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                     builder: (context) => PlantPage(plants[bed.plants[i]]!)));
+//           },
+//           trailing: Text(plants[bed.plants[i]]!.commonName)));
+//     }
 
-    return Scaffold(
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: ps,
-        ),
-        appBar: AppBar(title: Text(bed.title)));
-  }
-}
+//     return Scaffold(
+//         body: ListView(
+//           padding: const EdgeInsets.all(8),
+//           children: ps,
+//         ),
+//         appBar: AppBar(title: Text(bed.title)));
+//   }
+// }
 
-class PlantPage extends StatelessWidget {
-  Plant plant;
+// class PlantPage extends StatelessWidget {
+//   Plant plant;
 
-  PlantPage(this.plant) {
-    super.key;
-  }
+//   PlantPage(this.plant) {
+//     super.key;
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Row(children: [
-          Column(children: [
-            const Text("Common Name",
-                textAlign: TextAlign.left,
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            const Text("Scientific Name",
-                textAlign: TextAlign.left,
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            const Text("Recipes",
-                textAlign: TextAlign.left,
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            const Text("Description",
-                textAlign: TextAlign.left,
-                style: TextStyle(fontWeight: FontWeight.bold)),
-          ]),
-          Column(children: [
-            Text(plant.commonName, textAlign: TextAlign.right),
-            Text(plant.scientificName, textAlign: TextAlign.right),
-            Text(plant.recipes, textAlign: TextAlign.right),
-            Text(plant.description, textAlign: TextAlign.right),
-          ])
-        ]),
-        appBar: AppBar(title: Text(plant.commonName)));
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Row(children: [
+//           Column(children: [
+//             const Text("Common Name",
+//                 textAlign: TextAlign.left,
+//                 style: TextStyle(fontWeight: FontWeight.bold)),
+//             const Text("Scientific Name",
+//                 textAlign: TextAlign.left,
+//                 style: TextStyle(fontWeight: FontWeight.bold)),
+//             const Text("Recipes",
+//                 textAlign: TextAlign.left,
+//                 style: TextStyle(fontWeight: FontWeight.bold)),
+//             const Text("Description",
+//                 textAlign: TextAlign.left,
+//                 style: TextStyle(fontWeight: FontWeight.bold)),
+//           ]),
+//           Column(children: [
+//             Text(plant.commonName, textAlign: TextAlign.right),
+//             Text(plant.scientificName, textAlign: TextAlign.right),
+//             Text(plant.recipes, textAlign: TextAlign.right),
+//             Text(plant.description, textAlign: TextAlign.right),
+//           ])
+//         ]),
+//         appBar: AppBar(title: Text(plant.commonName)));
+//   }
+// }
 
 class ECMap extends StatefulWidget {
   @override
@@ -301,12 +294,12 @@ class ECMapState extends State<ECMap> {
   @override
   Widget build(BuildContext context) {
     Set<Marker> markers = {};
-    beds.values.forEach((bed) => markers.add(Marker(
+    data.gardens.values.forEach((bed) => markers.add(Marker(
         markerId: MarkerId(bed.title),
         position: LatLng(bed.latitude, bed.longitude),
         onTap: () {
           Navigator.push(context,
-              MaterialPageRoute(builder: (context) => GardenPage(bed)));
+              MaterialPageRoute(builder: (context) => data.plantPage(bed.key)));
         })));
 
     return Scaffold(
